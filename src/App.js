931f4eb4272d41f01ab959daf9947731d31f3088
Alberto Garcia <betogar@Albertos-MacBook-Pro.local>
2021-11-03 13:54:10 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World by Beto
         </p>
         <a
           className="App-link"
